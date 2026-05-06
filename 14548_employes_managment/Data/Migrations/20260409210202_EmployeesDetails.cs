17ca93a980245fc73080df4af66430dfb84f5689
Rodrigo Viana <142672337+rpviana@users.noreply.github.com>
2026-05-06 09:32:19 +0000

    changes

---
+++ 14548_employes_managment/Data/Migrations/20260409210202_EmployeesDetails.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _14548_employes_managment.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EmployeesDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmpNo = table.Column<string>(nullable: false),
+                    FirstName = table.Column<string>(nullable: false),
+                    MiddleName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    PhoneNumber = table.Column<string>(nullable: false),
+                    EmailAddress = table.Column<string>(nullable: false),
+                    Country = table.Column<string>(nullable: false),
+                    DateOfBirth = table.Column<DateTime>(nullable: false),
+                    Address = table.Column<string>(nullable: false),
+                    Department = table.Column<string>(nullable: false),
+                    Designation = table.Column<string>(nullable: false),
+                    CreatedById = table.Column<string>(nullable: true),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    ModifiedById = table.Column<string>(nullable: true),
+                    ModifiedAt = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
---
+++ 14548_employes_managment/Models/UserActivity.cs
@@ -0,0 +1,10 @@
+﻿namespace _14548_employes_managment.Models
+{
+    public class UserActivity
+    {
+        public string? CreatedById { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? ModifiedById { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
